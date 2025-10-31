7f4eceb837c1f66fd759744248ef488062208b18
ARSHAD637 <shaikarshad6856@gmail.com>
2025-10-31 12:50:12 +0530

    Add files via upload

---
+++ sudoku.py
@@ -0,0 +1,60 @@
+def is_valid(board, row, col, num):
+    # Row check
+    if num in board[row]:
+        return False
+    # Column check
+    if num in [board[i][col] for i in range(9)]:
+        return False
+    # 3x3 subgrid check
+    box_row = (row // 3) * 3
+    box_col = (col // 3) * 3
+    for i in range(box_row, box_row + 3):
+        for j in range(box_col, box_col + 3):
+            if board[i][j] == num:
+                return False
+    return True
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+def sudoku_solver(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    row, col = empty
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+            if sudoku_solver(board):
+                return True
+            board[row][col] = 0  # Backtrack
+    return False
+
+def print_board(board):
+    for row in board:
+        print(' '.join(str(num) for num in row))
+
+# Example puzzle (use any 9x9 grid, 0s for empty cells)
+board = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,7,9]
+]
+
+print("Original Sudoku:")
+print_board(board)
+if sudoku_solver(board):
+    print("\nSolved Sudoku grid:")
+    print_board(board)
+else:
+    print("No solution found.")
